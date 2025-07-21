ce66813788d77783b0e9038efee08b77101279f3
bbaserdem <baserdemb@gmail.com>
2025-07-21 15:38:25 -0500

    nix: Initial nix commit
    Added nix support for now.

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "Custom fork of LAPACK using lapack-reference derivation";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+    in {
+      packages.default = import ./nix/package.nix {inherit pkgs;};
+    });
+}
