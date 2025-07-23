a78521a971ca44575a88dd0c048ae3aad090a2f9
bbaserdem <baserdemb@gmail.com>
2025-07-23 14:21:50 -0500

    fix: move docker versions.
    Change to new docker spec

--- nix/claudeSwarm/docker.nix
+++ nix/claudeSwarm/docker.nix
@@ -3,19 +3,27 @@
 in
   pkgs.dockerTools.buildImage {
     name = "claude-flow-container-claudeSwarm";
-    contents = [
-      pkgs.nodejs-slim
-      pkgs.git
-      pkgs.bash
-      entrypoint
-    ];
     tag = "latest";
+    copyToRoot = pkgs.buildEnv {
+      name = "image-root";
+      paths = with pkgs; [
+        nodejs-slim_24
+        git
+        bash
+        coreutils
+        gawk
+        gnugrep
+        sed
+        entrypoint
+      ];
+      pathsToLink = ["/bin"];
+    };
     config = {
       WorkingDir = "/repo";
       Env = [
         # add any fixed env vars here, if needed
         # "EXAMPLE_ENV=VALUE"
       ];
-      Entrypoint = ["/bin/sh" entrypoint];
+      Entrypoint = ["/bin/entrypoint"];
     };
   }
