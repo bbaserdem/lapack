943e8356ef8e834e7e64b4a9101d3dc896105edc
bbaserdem <baserdemb@gmail.com>
2025-07-24 20:54:39 -0500

    feat: Added uv integration
    Added UV

--- nix/apps.nix
+++ nix/apps.nix
@@ -1,9 +1,20 @@
 # Scripts for different types of swarms
-{pkgs, ...}: let
+{
+  pkgs,
+  uvBoilerplate,
+  projectName,
+  outputs,
+  ...
+}: let
   claudeFlowDeploy = import ./claudeFlow/deploy.nix {inherit pkgs;};
 in {
   deploy-claudeFlow = {
     type = "app";
     program = "${claudeFlowDeploy}";
+  };
+  ${projectName} = {
+    type = "app";
+    # THIS NEEDS TO CHANGE
+    program = "${outputs.packages.${system}.default}/bin/${projectName}";
   };
 }
